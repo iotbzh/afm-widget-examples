32fce3b5b1ae40a93b33e59ab47f1592b30a5b7a
José Bollo <jose.bollo@iot.bzh>
2018-03-13 18:35:59 +0100

    build.sh: Fix an issue of the main of memory-match
    Change-Id: I021105845332b66ba082cd66caddf45ea2c63a66
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i s:switch.dl.sourceforge.net:downloads.sourceforge.net: modules/webapps-annex/bower.json
+sed -i '/"main":/s:/js/main.js:/index.html:' modules/webapps-memory-match/package.json
 ./pack-gulp.sh annex
 ./pack-gulp.sh rabbit
 ./pack-gulp.sh memory-match
